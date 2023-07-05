086769e05536dd06e02674f57b09f474eea93108
Rodrigo Riveros <riverosdev@gmail.com>
2023-07-05 00:47:27 -0300

    agrego estilos y contenido a nosotros y tienda

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.static('public'));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/nosotros', (req,res) => res.sendFile(path.join(__dirname, 'views', 'nosotros.html')));
 app.get('/tienda', (req,res) => res.sendFile(path.join(__dirname, 'views', 'tienda.html')));
-app.get('/blog', (req,res) => res.sendFile(path.join(__dirname, 'views', 'blog.html')));
 app.get('/galeria', (req,res) => res.sendFile(path.join(__dirname, 'views', 'galeria.html')));
 app.get('/contacto', (req,res) => res.sendFile(path.join(__dirname, 'views', 'contacto.html')));
 
